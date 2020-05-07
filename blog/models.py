706034fe5abc8f3b89740ceed61cc4b34d81cc45
holic <elric2009@gmail.com>
2020-05-07 20:23:37 +1000

    first commit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.shortcuts import reverse
 
-# Create your models here.
+from django.utils.text import slugify
+from time import time
+
+def gen_slug(s):
+    new_slug = slugify(s, allow_unicode=True)
+    return new_slug + '-' + str(int(time()))
+
+class Post(models.Model):
+    title = models.CharField(max_length=150, db_index=True)
+    slug = models.SlugField(max_length=150, blank=True, unique=True)    #slug - человекопонятный url
+    body = models.TextField(blank=True, db_index=True)   #blank=True - это поле может быть пустым, db_index - флаг индексации (можно искать по содержанию)
+    date_pub = models.DateTimeField(auto_now_add=True)      #при сохранении публикации в базу данных, будет выставляться текущая дата
+    tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
+
+    def get_absolute_url(self):
+        return reverse("post_detail_url", kwargs={"slug": self.slug})
+
+    def get_update_url(self):
+        return reverse("post_update_url", kwargs={"slug": self.slug})
+
+    def get_delete_url(self):
+        return reverse("post_delete_url", kwargs={"slug": self.slug})
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = gen_slug(self.title)
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['-date_pub']
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def get_absolute_url(self):
+        return reverse("tag_detail_url", kwargs={"slug": self.slug})
+
+    def get_update_url(self):
+        return reverse("tag_update_url", kwargs={"slug": self.slug})
+
+    def get_delete_url(self):
+        return reverse("tag_delete_url", kwargs={"slug": self.slug})
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
